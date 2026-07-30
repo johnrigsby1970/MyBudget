d9a9069bc63eacbfa94764ff80cac4741af27560
John Rigsby <jrigsby@tcrdev.com>
2026-07-30 19:56:56 -0400

    Fix warning

--- StayOnTarget/Converters/EnumToBooleanConverter.cs
+++ StayOnTarget/Converters/EnumToBooleanConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
@@ -6,18 +7,25 @@ namespace StayOnTarget.Converters;
 
 public class EnumToBooleanConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value == null || parameter == null) return false;
-        return value.ToString() == parameter.ToString();
+
+        return string.Equals(value.ToString(), parameter.ToString(), StringComparison.OrdinalIgnoreCase);
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is bool b && b && parameter != null)
+        if (value is true && parameter != null)
         {
-            return Enum.Parse(targetType, parameter.ToString());
+            string? paramString = parameter.ToString();
+            
+            if (!string.IsNullOrEmpty(paramString) && Enum.IsDefined(targetType, paramString))
+            {
+                return Enum.Parse(targetType, paramString, ignoreCase: true);
+            }
         }
+
         return DependencyProperty.UnsetValue;
     }
 }
